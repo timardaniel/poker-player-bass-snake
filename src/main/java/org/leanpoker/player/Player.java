f8a09187d5fa6ebe632969b188119231b23e38c2
timardaniel <daniel.timar21@gmail.com>
2018-10-01 10:48:29 +0200

    bet 10

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -6,8 +6,8 @@ public class Player {
     static final String VERSION = "Default Java folding player";
 
     public static int betRequest(JsonElement request) {
-        System.out.println(JsonElement);
-        return 1;
+        System.out.println(request);
+        return 10;
     }
 
     public static void showdown(JsonElement game) {
