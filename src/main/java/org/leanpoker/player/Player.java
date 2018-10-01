a9b1e6ef5d780e20594118bf34163708a04f2069
timardaniel <daniel.timar21@gmail.com>
2018-10-01 10:55:26 +0200

    bet 100

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -3,7 +3,7 @@ package org.leanpoker.player;
 import com.google.gson.JsonElement;
 public class Player {
 
-    static final String VERSION = "Default Java folding player";
+    static final String VERSION = "1.1";
 
     public static int betRequest(JsonElement request) {
         System.out.println(request);
