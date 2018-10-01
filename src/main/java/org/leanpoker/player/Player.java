689e3baa3658d2c323b2bfd9fc5f42d3227f1b4d
David Schuppa <d.schuppa@gmail.com>
2018-10-01 14:00:49 +0200

    json parsed

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -10,12 +10,11 @@ public class Player {
     static final String VERSION = "1.1";
 
     public static int betRequest(JsonElement request) {
-//        JsonObject json = request.getAsJsonObject();
-        Map jsonJavaRootObject = new Gson().fromJson(request, Map.class);
-        System.err.println("Hello");
-        System.err.println(jsonJavaRootObject.get("hole_cards").toString());
+        Map<String,Object> result = new Gson().fromJson(request, Map.class);
+        System.err.println(result.get("players"));
 
-        return 200;
+
+        return 10000;
     }
 
     public static void showdown(JsonElement game) {
