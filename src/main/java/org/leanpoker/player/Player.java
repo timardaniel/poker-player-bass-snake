4996056581f44d26713316beb7d2c2ea29140733
timardaniel <daniel.timar21@gmail.com>
2018-10-01 13:42:30 +0200

    bet 200 version 1.1

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -1,9 +1,6 @@
 package org.leanpoker.player;
 
-import com.google.gson.Gson;
 import com.google.gson.JsonElement;
-
-import java.util.Map;
 
 public class Player {
 
@@ -11,10 +8,10 @@ public class Player {
 
     public static int betRequest(JsonElement request) {
 //        JsonObject json = request.getAsJsonObject();
-        Map jsonJavaRootObject = new Gson().fromJson(request, Map.class);
+       /* Map jsonJavaRootObject = new Gson().fromJson(request, Map.class);
         System.err.println("Hello");
         System.err.println(jsonJavaRootObject.get("hole_cards").toString());
-
+*/
         return 500;
     }
 
