9f464fbc73a69eee0b70f1461a12feda8d4526a6
Szajler Indira <szajler.indira@gmail.com>
2018-10-01 14:08:32 +0200

    Merge branch 'master' of https://github.com/timardaniel/poker-player-bass-snake
    # Conflicts:
    #	src/main/java/org/leanpoker/player/Player.java

--- src/main/java/org/leanpoker/player/Card.java
+++ src/main/java/org/leanpoker/player/Card.java
@@ -7,4 +7,21 @@ public class Card {
     public static ArrayList<HashMap<String, String>> holeCards = new ArrayList<>();
     public static ArrayList<HashMap<String, String>> communityCards = new ArrayList<>();
 
+    public static boolean isThereAPair() {
+        if (holeCards.get(0).get("rank").equals(holeCards.get(1).get("rank")))
+            return true;
+
+        for (HashMap<String, String> card: communityCards) {
+            if (card.get("rank").equals(holeCards.get(0).get("rank"))
+                    || card.get("rank").equals(holeCards.get(1).get("rank")))
+                return true;
+        }
+        return false;
+    }
+
+    public static boolean areCardsSmall() {
+        if (Integer.parseInt(holeCards.get(0).get("rank")) < 7 && Integer.parseInt(holeCards.get(1).get("rank")) < 7);
+        return true;
+    }
+
 }
