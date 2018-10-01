fb97e3e718314b78758b738aa5323e0040f83e52
Uj Levente <asdlevente@gmail.com>
2018-10-01 10:38:04 +0200

    print added

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -9,11 +9,12 @@ public class Player {
     static final String VERSION = "Default Java folding player";
 
     public static int betRequest(JsonElement request) {
-        System.out.println(JsonElement);
+        //System.out.println(JsonElement);
         return 0;
     }
 
     public static void showdown(JsonElement game) {
+        System.out.println("jo asd");
     }
 }
 
