a2b8e5d51e063c2d82d311fdff40bb6f09f43cd0
Szajler Indira <szajler.indira@gmail.com>
2018-10-01 15:28:20 +0200

    logic added

--- src/main/java/org/leanpoker/player/Card.java
+++ src/main/java/org/leanpoker/player/Card.java
@@ -7,21 +7,57 @@ public class Card {
     public static ArrayList<HashMap<String, String>> holeCards = new ArrayList<>();
     public static ArrayList<HashMap<String, String>> communityCards = new ArrayList<>();
 
-    public static boolean isThereAPair() {
-        if (holeCards.get(0).get("rank").equals(holeCards.get(1).get("rank")))
-            return true;
+    public static boolean isThereAPairInHand() {
+        return (holeCards.get(0).get("rank").equals(holeCards.get(1).get("rank")));
+    }
 
-        for (HashMap<String, String> card: communityCards) {
-            if (card.get("rank").equals(holeCards.get(0).get("rank"))
-                    || card.get("rank").equals(holeCards.get(1).get("rank")))
-                return true;
+    public static int checkOccurences(String rank) {
+        int counter = 0;
+        for (HashMap<String, String> card : communityCards) {
+            if (card.get("rank").equals(rank))
+                counter++;
         }
+        return counter;
+    }
+
+
+    public static int chechNumberOfCardsChance() {
+        if (isThereAPairInHand()) {
+            int counter = checkOccurences(holeCards.get(0).get("rank"));
+            switch (counter) {
+                case 0:
+                    return 4;
+                case 1:
+                    return 8;
+                case 2:
+                    return 10;
+            }
+
+        } else {
+            int counter1 = checkOccurences(holeCards.get(0).get("rank"));
+            int counter2 = checkOccurences(holeCards.get(1).get("rank"));
+            int count = Math.max(counter1, counter2);
+
+            switch (count) {
+                case 0:
+                    return 0;
+                case 1:
+                    return 3;
+                case 2:
+                    return 7;
+                case 3:
+                    return 9;
+
+            }
+
+        }
+    }
+
+
+
+    public static boolean areCardsSmall () {
+        if (Integer.parseInt(holeCards.get(0).get("rank")) < 7 && Integer.parseInt(holeCards.get(1).get("rank")) < 7)
+            return true;
         return false;
     }
-
-    public static boolean areCardsSmall() {
-        if (Integer.parseInt(holeCards.get(0).get("rank")) < 7 && Integer.parseInt(holeCards.get(1).get("rank")) < 7);
-        return true;
-    }
-
 }
--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -9,7 +9,13 @@ public class Player {
     public static int betRequest(JsonElement request) {
         Map<String,Object> result = new Gson().fromJson(request, Map.class);
         System.err.println(result.get("players"));
-        return 10000;
+
+
+        if (Card.chechNumberOfCardsChance() > 0 ) {
+            return 10000;
+        } else {
+            return Card.areCardsSmall() ? 0 : 10000;
+       }
     }
 
     public static void showdown(JsonElement game) {
